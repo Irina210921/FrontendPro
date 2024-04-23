337494a8b9533114dc127deab2005fbc8ba22ef5
Irina210921 <pux32008201@gmail.com>
2024-04-23 18:15:36 +0300

    new file file script first homework completed three homeworks 1 and 3 homework-ok 2 homework-ok redid the homework 3.3 and 3.2 homework-index4.1 homework -index.4.2 homework-index4.4 variant#2 homework 4.2 homework 4.3 new js4.2 homework 4.4

---
+++ homework 4/home4/js4.4.js
@@ -0,0 +1,20 @@
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
+
+switch (numOrStr){
+
+    case '': console.log('Empty String');
+    break;
+    case 'world': console.log('number is Ba_NaN');
+    break;
+    default: console.log('OK!');
+    break;
+}
+
+
+
+
+
+
+
+
