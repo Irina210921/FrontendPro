dac81a17ab6d9da5ca4cce28e5b75ff205199d05
Irina210921 <pux32008201@gmail.com>
2024-04-26 22:58:27 +0300

    new file file script first homework completed three homeworks 1 and 3 homework-ok 2 homework-ok redid the homework 3.3 and 3.2 homework-index4.1 homework -index.4.2 homework-index4.4 variant#2 homework 4.2 homework 4.3 new js4.2 homework 4.4 homework 5.1 homework 5.2 homework 5.3 homework 5.4

---
+++ homework5/js5.4.js
@@ -0,0 +1,28 @@
+
+
+let num = prompt('Введіть число');
+console.log(num);
+
+for (let i=2; i < num; i++) {
+       if (num % i === 0){
+              console.log('not a prime number')
+       }
+       else {
+              console.log('prime number')
+       }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
