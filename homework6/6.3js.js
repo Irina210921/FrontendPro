9c3409b2734bcb3fcab9af5c25893048cbcf8826
Irina210921 <pux32008201@gmail.com>
2024-04-29 17:02:52 +0300

    new file file script first homework completed three homeworks 1 and 3 homework-ok 2 homework-ok redid the homework 3.3 and 3.2 homework-index4.1 homework -index.4.2 homework-index4.4 variant#2 homework 4.2 homework 4.3 new js4.2 homework 4.4 homework 5.1 homework 5.2 homework 5.3 homework 5.4 homework 6.3 6.3 dubl2

--- homework6/6.3js.js
+++ homework6/6.3js.js
@@ -1,5 +1,14 @@
+
 const array = [1, 3, 4, 6, 2, 5, 7];
+function removeElement () {
 
-const newArray  = array.splice(2, 2);
+    const newArray = array.splice(4, 1)
+    console.log(array);
 
-console.log(newArray);
+}
+
+removeElement();
+
+
+
+
