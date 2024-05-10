afd3658df1f8cf17296c2d53509263afc8dbeceb
Irina210921 <pux32008201@gmail.com>
2024-05-10 12:53:04 +0300

    new file file script first homework completed three homeworks 1 and 3 homework-ok 2 homework-ok redid the homework 3.3 and 3.2 homework-index4.1 homework -index.4.2 homework-index4.4 variant#2 homework 4.2 homework 4.3 new js4.2 homework 4.4 homework 5.1 homework 5.2 homework 5.3 homework 5.4 homework 6.3 6.3 dubl2 6.3 dubl3 homework 6.2 homework 8.1 homework 7.2 homework 7.3 homework 6.1

--- homework6/6.1 js.js
+++ homework6/6.1 js.js
@@ -1,10 +1,20 @@
-        let text = 'Hello word';
 
 
-            let x = text.replace(/[ld]/g, '');
+let text = 'Hello word';
+function textReplace (text, item){
+
+    let x = text.replace(/[ld]/g, '');
+    return x
+}
 
 
-        console.log(x);
+
+console.log(textReplace(text));
+
+
+
+
+
 
 
 
