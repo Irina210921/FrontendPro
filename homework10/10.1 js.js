2c3bfa63a7330da332e50d6ff89c2d70c3d397e9
Irina210921 <pux32008201@gmail.com>
2024-05-13 16:48:30 +0300

    new file file script first homework completed three homeworks 1 and 3 homework-ok 2 homework-ok redid the homework 3.3 and 3.2 homework-index4.1 homework -index.4.2 homework-index4.4 variant#2 homework 4.2 homework 4.3 new js4.2 homework 4.4 homework 5.1 homework 5.2 homework 5.3 homework 5.4 homework 6.3 6.3 dubl2 6.3 dubl3 homework 6.2 homework 8.1 homework 7.2 homework 7.3 homework 6.1 homework 6.1 updated homework 10.1 and 10.2

---
+++ homework10/10.1 js.js
@@ -0,0 +1,9 @@
+let user ={
+    name: "Iryna",
+    age: "35",
+    city: "Dnipro"
+
+}
+console.log(user.name);
+console.log(user.age);
+console.log(user.city);
---
+++ homework10/10.2 js.js
@@ -0,0 +1,6 @@
+const array = [1, 2, 3, 4, 5, 6]
+
+
+const newArray = array.filter(num => num % 2 === 0);
+
+console.log(newArray);
