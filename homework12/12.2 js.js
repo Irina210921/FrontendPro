edee10c4f28a323e3e0a3a18fdd0857e24587ba3
Irina210921 <pux32008201@gmail.com>
2024-05-27 22:16:15 +0300

    new file file script first homework completed three homeworks 1 and 3 homework-ok 2 homework-ok redid the homework 3.3 and 3.2 homework-index4.1 homework -index.4.2 homework-index4.4 variant#2 homework 4.2 homework 4.3 new js4.2 homework 4.4 homework 5.1 homework 5.2 homework 5.3 homework 5.4 homework 6.3 6.3 dubl2 6.3 dubl3 homework 6.2 homework 8.1 homework 7.2 homework 7.3 homework 6.1 homework 6.1 updated homework 10.1 and 10.2 homework 9.1+ homework 10.3 homework 11.2 homework pifagor homework 12.1 homework 11.3 homework 12.2 homework 12.3 homework 12.2 dubl2

--- homework12/12.2 js.js
+++ homework12/12.2 js.js
@@ -1,8 +1,29 @@
-
-        document.addEventListener('click', function(event) {
-
-        if (event.target.dataset.counter != undefined) {
-        event.target.value++;
+class Menu {
+    constructor(elem) {
+        this._elem = elem;
+        elem.onclick = this.onClick.bind(this);
     }
 
-    });
+    button1() {
+        alert('натиснута кнопка1');
+    }
+
+    button2() {
+        alert('натиснута кнопка 2');
+    }
+
+    button3() {
+        alert('натиснута кнопка 3');
+    }
+
+    onClick(event) {
+        let action = event.target.dataset.action;
+        if (action) {
+            this[action]();
+        }
+    };
+}
+
+new Menu(menu);
+
+
