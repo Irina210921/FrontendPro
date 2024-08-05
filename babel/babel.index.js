d21f6109841001fb94233d38e7d9774a64a4febd
Irina210921 <pux32008201@gmail.com>
2024-08-05 22:44:04 +0300

    new file file script first homework completed three homeworks 1 and 3 homework-ok 2 homework-ok redid the homework 3.3 and 3.2 homework-index4.1 homework -index.4.2 homework-index4.4 variant#2 homework 4.2 homework 4.3 new js4.2 homework 4.4 homework 5.1 homework 5.2 homework 5.3 homework 5.4 homework 6.3 6.3 dubl2 6.3 dubl3 homework 6.2 homework 8.1 homework 7.2 homework 7.3 homework 6.1 homework 6.1 updated homework 10.1 and 10.2 homework 9.1+ homework 10.3 homework 11.2 homework pifagor homework 12.1 homework 11.3 homework 12.2 homework 12.3 homework 12.2 dubl2 homework 12.2 dubl3 homework 13.1 homework 14 homework 15.1 todo homework 18.1 timer homework 19 weather homework 19 weather + loading homework 17.1 calc homework 17.1 calc updated homework 17.1 trener homework 20.1 jqery homework 20.1 jqery + bootstrap homework 17.3 Banc homework 16 homework 21.babel

---
+++ babel/babel.index.js
@@ -0,0 +1,38 @@
+const input = document.querySelector('.form__input');
+const todoListUl = document.querySelector('.js-todos-wrapper');
+const button = document.querySelector('.form__btn');
+
+let todos =[];
+function toLocal(){
+    todos = todoListUl.innerHTML;
+    localStorage.setItem('todos', todos);
+}
+
+button.addEventListener("click", () => {
+    const li = document.createElement('li');
+    li.className = 'todo-item';
+    const btnDelete = document.createElement('button');
+    li.innerText = input.value;
+    todoListUl.appendChild(li);
+    li.appendChild(btnDelete);
+    btnDelete.innerText = 'Видалити';
+
+    input.value = ""
+    input.focus()
+    toLocal();
+
+
+
+    btnDelete.addEventListener('click', () =>{
+        todoListUl.removeChild(li);
+        toLocal();
+    })
+
+    li.addEventListener('click', (e) =>{
+        li.classList.toggle('li-active');
+        toLocal();
+    })
+})
+if (localStorage.getItem('todos')){
+    todoListUl.innerHTML = localStorage.getItem('todos')
+}
